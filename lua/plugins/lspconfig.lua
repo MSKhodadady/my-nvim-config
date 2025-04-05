3ea0e223bd5ac654b4fdeae5bceb007af361a3fd
mohammad-sadeq-khodadadei <m.s.khodadadei@gmail.com>
2025-04-05 23:45:42 +0330

    vim global variableOF

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,9 +10,33 @@ return {
 
 	-- Automatically set up all installed servers
 	require("mason-lspconfig").setup_handlers {
-	    function(server_name)
-		lspconfig[server_name].setup {}
-	    end
+	    	function(server_name)
+			lspconfig[server_name].setup {}
+	    	end,
+		["lua_ls"] = function ()
+			lspconfig.lua_ls.setup{
+				settings = {
+				    Lua = {
+					runtime = {
+					    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					    version = "LuaJIT",
+					},
+					diagnostics = {
+					    -- Get the language server to recognize the `vim` global
+					    globals = { "vim" },
+					},
+					workspace = {
+					    -- Make the server aware of Neovim runtime files
+					    library = vim.api.nvim_get_runtime_file("", true),
+					},
+					-- Do not send telemetry data containing a randomized but unique identifier
+					telemetry = {
+					    enable = false,
+					},
+				    },
+				}
+			}
+		end
 	}
   end,
 
